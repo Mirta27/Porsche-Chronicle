601c66f8a0702d0c710dfca7420be5923a6cc39d
Juho A. Kronbäck <juho.back.96@gmail.com>
2020-03-01 16:52:41 +0200

    Added if-statements to load Porsches based on user's family selection

--- js/timeline.js
+++ js/timeline.js
@@ -24,13 +24,14 @@ Porsche.prototype.toString = function () {
     return "Porsche no. " + this.serial + " is a " + this.modelName + " of " + this.modelYear + " with an id of " + this.id + ". It uses the showcase of " + this.showcaseId + " and is part of the " + this.family + " line.";
 }
 
-function draw(index) {
+function draw(index1, index2) {
     document.getElementById("porscheYear").innerHTML = this.modelYear;
     document.getElementById("porscheModel").innerHTML = "Porsche " + this.modelName;
     document.getElementById("porscheImageContainer").style.backgroundImage = "url(images/" + this.id + ".jpg)";
 
     document.getElementById("yearSelector").value = this.serial;
-    currentDataBaseIndexHorizontal = index;
+    currentDataBaseIndexHorizontal = index1;
+    currentDataBaseIndexVertical = index2;
     console.log(currentDataBaseIndexHorizontal + ", " + currentDataBaseIndexVertical);
     updateUpDownArrows();
 }
@@ -51,41 +52,47 @@ var porscheModel = ["914", "968", "928 GTS", "911 Carrera S (997)"];
 
 /* loading all porsches found and turning them into Porsche objects */
 /* the Porsche array goes in two dimensions, cars of the same year are added as an array for that year */
-function loadPorsches() {
+function loadPorsches(wantedFamily) {
     for (var i = 0; i < porscheSerial.length; i++) {
         var currentPorsche = new Porsche(porscheSerial[i], porscheId[i], porscheShowcase[i], porscheFamily[i], porscheYear[i], porscheModel[i]);
+        /* the current Porsche needs to be of the model family the user wants to see on the website */
+        if(currentPorsche.family == wantedFamily || wantedFamily == "all") {
 
-        /* if numerous Porsches of the same year were found or not (assumes the database is in order) */
-        if(porscheSerial[i+1] == porscheSerial[i]) {
-            var begin = i+1;
-            var tempArray = [currentPorsche];
+            /* if numerous Porsches of the same year were found or not (assumes the database is in order) */
+            if(porscheSerial[i+1] == porscheSerial[i]) {
+                var begin = i+1;
+                var tempArray = [currentPorsche];
 
-            /* checking exactly how many future Porsches in line have the same year */
-            for(var j = begin; j < porscheSerial.length; j++) {
-                if(porscheSerial[j] == porscheSerial[j-1]) {
-                    tempArray.push(new Porsche(porscheSerial[j], porscheId[j], porscheShowcase[j], porscheFamily[j], porscheYear[j], porscheModel[j]));
-                    i++;
-                } else {
-                    break;
+                for(var j = begin; j < porscheSerial.length; j++) {
+                    if(porscheSerial[j] == porscheSerial[j-1]) {
+                        var nextPorsche = new Porsche(porscheSerial[j], porscheId[j], porscheShowcase[j], porscheFamily[j], porscheYear[j], porscheModel[j]);
+                        if(nextPorsche.family == wantedFamily || wantedFamily == "all") {
+                            tempArray.push(nextPorsche);
+                        }
+                        i++;
+                    } else {
+                        break;
+                    }
                 }
+                if(tempArray.length > 1) {
+                    porscheModels.push(tempArray);
+                } else {
+                    porscheModels.push(currentPorsche);
+                }
+            } else {
+                porscheModels.push(currentPorsche);
             }
-            porscheModels.push(tempArray);
-        } else {
-            porscheModels.push(currentPorsche);
         }
     }
     console.log(porscheModels);
 }
 
 /* selects the default car of current settings */
-function selectDefault(family) {
-    switch(family) {
-        default:
-            if(porscheModels[family].length > 1) {
-                porscheModels[family][0].draw(family);
-            } else {
-                porscheModels[family].draw(family);
-            }
+function selectDefault() {
+    if(porscheModels[0].length > 1) {
+        porscheModels[0][0].draw(0, 0);
+    } else {
+        porscheModels[0].draw(0, 0);
     }
 }
 
@@ -108,11 +115,11 @@ function updateUpDownArrows() {
         if(porscheModels[currentDataBaseIndexHorizontal][currentDataBaseIndexVertical-1] != null && porscheModels[currentDataBaseIndexHorizontal][currentDataBaseIndexVertical+1] != null) {
             console.log("Showing up and down arrows.");
             upDownArrows("block", "block");
-        /* has above but not below? */
+            /* has above but not below? */
         } else if(porscheModels[currentDataBaseIndexHorizontal][currentDataBaseIndexVertical-1] != null) {
             console.log("Showing up arrow.");
             upDownArrows("block", "none");
-        /* must only have below */
+            /* must only have below */
         } else {
             console.log("Showing down arrow.");
             upDownArrows("none", "block");
@@ -134,19 +141,17 @@ function moveSelectionDown() {
 function moveSelection(direction) {
     switch(direction) {
         case "up":
-            currentDataBaseIndexVertical--;
-            porscheModels[currentDataBaseIndexHorizontal][currentDataBaseIndexVertical].draw(currentDataBaseIndexHorizontal);
+            porscheModels[currentDataBaseIndexHorizontal][currentDataBaseIndexVertical-1].draw(currentDataBaseIndexHorizontal, currentDataBaseIndexVertical-1);
             break;
         case "down":
-            currentDataBaseIndexVertical++;
-            porscheModels[currentDataBaseIndexHorizontal][currentDataBaseIndexVertical].draw(currentDataBaseIndexHorizontal);
+            porscheModels[currentDataBaseIndexHorizontal][currentDataBaseIndexVertical+1].draw(currentDataBaseIndexHorizontal, currentDataBaseIndexVertical+1);
             break;
         case "zero":
             currentDataBaseIndexVertical = 0;
             break;
         default:
             currentDataBaseIndexVertical = 0;
-            porscheModels[currentDataBaseIndexHorizontal][currentDataBaseIndexVertical].draw(currentDataBaseIndexHorizontal);
+            porscheModels[currentDataBaseIndexHorizontal][currentDataBaseIndexVertical].draw(currentDataBaseIndexHorizontal, currentDataBaseIndexVertical);
     }
 }
 
@@ -162,12 +167,12 @@ function yearChange() {
     for(var i = 0; i < porscheModels.length; i++) {
         if(porscheModels[i].length > 1) {
             if(porscheModels[i][0].serial == wantedSerial) {
-                porscheModels[i][0].draw(i);
+                porscheModels[i][0].draw(i, 0);
                 upDownArrows("none", "block");
             }
         } else {
             if(porscheModels[i].serial == wantedSerial) {
-                porscheModels[i].draw(i);
+                porscheModels[i].draw(i, 0);
                 upDownArrows("none", "none");
             }
         }
@@ -180,10 +185,10 @@ function yearChange() {
 
 
 /* main loop */
-loadPorsches();
+loadPorsches("all");
 
 window.onload = function() {
-    selectDefault(0);
+    selectDefault();
     document.getElementById("yearSelector").addEventListener('input', yearChange);
     var upArrows = document.getElementsByClassName("upArrows");
     var downArrows = document.getElementsByClassName("downArrows");
