829265a7ebf49fbda5c9c8ead6e132eb2446b9dd
Juho A. Kronbäck <juho.back.96@gmail.com>
2020-05-20 17:47:00 +0300

    All four showcase tabs are now functional with 944 S2 data.

--- js/showcase.js
+++ js/showcase.js
@@ -2,7 +2,7 @@
 /* I'll try to not have example cars this time. Perhaps one, but I want to prioritize loading them from the database from the outset. */
 
 /* This object houses all the information of the showcased car. */
-function PorscheShowcase(modelName, overview, specs, imageLimitGen, imageLimitSml, imageLimitCut, imageLimitEng, imageLimitBea, videoLimit) {
+function PorscheShowcase(modelName, overview, specs, imageLimitGen, imageLimitSml, imageLimitCut, imageLimitEng, imageLimitBea, videos, videoLimit) {
     this.modelName = modelName;
     this.overview = overview;
     this.specs = specs;
@@ -11,8 +11,8 @@ function PorscheShowcase(modelName, overview, specs, imageLimitGen, imageLimitSm
     this.imageLimitCut = imageLimitCut;
     this.imageLimitEng = imageLimitEng;
     this.imageLimitBea = imageLimitBea;
+    this.videos = videos;
     this.videoLimit = videoLimit;
-    /* attempting to follow the Command design pattern, the Showcase commands itself to be drawn on screen. */
 }
 
 PorscheShowcase.prototype.toString = function () {
@@ -25,7 +25,8 @@ var amountOfBigImages = 1;
 var amountOfSmallImages = 1;
 var currentBigImage = 1;
 var currentSmallImage = 1;
-var currentPage = 0;
+
+var displayMode = "normal";
 var currentPageIds = [];
 var currentHeader = [];
 var currentShowcase = [];
@@ -35,12 +36,14 @@ var maxPageNumber = 1;
 /* The single most important function in the entire website. This function applies all the data found from the database into the showcaseCar object. */
 /* The data array is hardcoded until I code the database stuff that loads data, similar to the timeline page. */
 function constructShowcase(currentCar) {
-    var data = ["Porsche 944 S2 '89", "Porsche/sqs golden era comes in golden proportions. Behold: the golden child of many an automotive enthusiast, the dearest front-engined Porsche of them all, the 944. Introduced as the 924/sqs replacement in 1982, the 944 featured a Porsche engine in a Porsche car, a standout feature compared to the 924./r/nThe 944/sqs inline-four-cylinder engine was adapted from the 928/sqs 4.5-litre alloy V8. It was dismembered into a half-V8, and slanted over at 45 degrees to the right during installation. This engine featured Mitsubishi/sqs dual counter-rotating engine balancing shafts to counter the unbalanced secondary forces present in inline engines. The engine had a displacement of 2.5 litres and produced 163 bhp upon its EU debut and only 143 bhp in the US. Overall, the 944 was essentially a 924 with a premium engine and trim, making it a much more desireable entry-level Porsche in the eyes of many consumers./r/n1989 saw the dawn of the 944 S2, which features all the facelifts the 944 saw in 1985, complete with new styling and a brand-new interior. The S2 is an upgraded version of the 944 S, offering the largest four-cylinder engine of the 1980s: a 3.0-litre DOHC 16-valve inline-four producing 208 bhp, coupled to a revised transmission optimized for maximum performance. The 944 S2 delivered a 0-60 time of 6.8 seconds and a top speed of 150 mph. It could be optioned with dual air bags, a limited slip differential and ABS brakes, as well as the Club Sport touring package. Design 90 16/dq cast alloy wheels and Turbo-look styling elements were offered as standard./r/nAmong the exclusively front-engined /dqgreatest handling Porsches of all time/dq-club, the 944 reigns supreme, and it/sqs no wonder when its 50.7/49.3 weight distribution and beautiful suspension result in perfectly balanced handling.", null, null, null, 3, 4, 1, 5, 2, 1];
+    var data = ["Porsche 944 S2 '89", "Porsche/sqs golden era comes in golden proportions. Behold: the golden child of many an automotive enthusiast, the dearest front-engined Porsche of them all, the 944. Introduced as the 924/sqs replacement in 1982, the 944 featured a Porsche engine in a Porsche car, a standout feature compared to the 924./r/nThe 944/sqs inline-four-cylinder engine was adapted from the 928/sqs 4.5-litre alloy V8. It was dismembered into a half-V8, and slanted over at 45 degrees to the right during installation. This engine featured Mitsubishi/sqs dual counter-rotating engine balancing shafts to counter the unbalanced secondary forces present in inline engines. The engine had a displacement of 2.5 litres and produced 163 bhp upon its EU debut and only 143 bhp in the US. Overall, the 944 was essentially a 924 with a premium engine and trim, making it a much more desireable entry-level Porsche in the eyes of many consumers./r/n1989 saw the dawn of the 944 S2, which features all the facelifts the 944 saw in 1985, complete with new styling and a brand-new interior. The S2 is an upgraded version of the 944 S, offering the largest four-cylinder engine of the 1980s: a 3.0-litre DOHC 16-valve inline-four producing 208 bhp, coupled to a revised transmission optimized for maximum performance. The 944 S2 delivered a 0-60 time of 6.8 seconds and a top speed of 150 mph. It could be optioned with dual air bags, a limited slip differential and ABS brakes, as well as the Club Sport touring package. Design 90 16/dq cast alloy wheels and Turbo-look styling elements were offered as standard./r/nAmong the exclusively front-engined /dqgreatest handling Porsches of all time/dq-club, the 944 reigns supreme, and it/sqs no wonder when its 50.7/49.3 weight distribution and beautiful suspension result in perfectly balanced handling.", null, null, null, 3, 4, 1, 5, 3, "https://www.youtube.com/embed/EBRWHtXW_7E", "https://www.youtube.com/embed/s6zumyZddmc", null, null];
 
     var specs = ["Contents", "left", "to", "do", "here"];
 
     var fixedShowcases = [];
     var fixedData = [];
+    var videos = [];
+    var videoAmount = 0;
 
     for(var i = 1; i <= 4; i++) {
         var showcase = null;
@@ -62,6 +65,13 @@ function constructShowcase(currentCar) {
         fixedData.push(spec);
     }
 
+    for(var i = 10; i <= 13; i++) {
+        if(data[i] != null) {
+            videos.push(data[i]);
+            videoAmount++;
+        }
+    }
+
     showcaseCar.modelName = data[0];
     showcaseCar.overview = fixedShowcases;
     showcaseCar.specs = fixedData;
@@ -70,17 +80,23 @@ function constructShowcase(currentCar) {
     showcaseCar.imageLimitCut = data[7];
     showcaseCar.imageLimitEng = data[8];
     showcaseCar.imageLimitBea = data[9];
-    showcaseCar.videoLimit = data[10];
+    showcaseCar.videos = videos;
+    showcaseCar.videoLimit = videoAmount;
 
     console.log(showcaseCar.toString());
+}
+
+/* It's unnecessary to call this every time tabs are changed. */
+function setHeader() {
+    document.getElementById("porscheYear").innerHTML = showcaseCar.modelName;
 }
 
 /* The data developed in the previous function is printed onto the page here */
 function initializeShowcase() {
     currentPageNumber = 1;
-    document.getElementById("porscheYear").innerHTML = showcaseCar.modelName;
 
     /* If type = true, the page wants the overview showcase. If false, it wants specs. */
+    document.getElementById("porscheVideo").src = showcaseCar.videos[0];
     document.getElementById("porscheShowcase").innerHTML = currentShowcase[0];
     if(currentHeader instanceof Array) {
         document.getElementById("porscheHeader").innerHTML = currentHeader[0];
@@ -88,39 +104,65 @@ function initializeShowcase() {
         document.getElementById("porscheHeader").innerHTML = currentHeader;
     }
 
-    findShowcaseImageOfType(true, currentCar, 1);
-    findShowcaseImageOfType(false, currentCar, 1);
+    console.log("Display mode: " + displayMode);
+    if(displayMode != "video") {
+        findShowcaseImageOfType(true, currentCar, 1);
+        if(displayMode == "normal") {
+            findShowcaseImageOfType(false, currentCar, 1);
+        }
+    }
 
     updatePageSelector();
 }
 
-/* This function changes pages. */
+/* This function initially calculates the maximum amount of pages available in the showcase. */
+function updatePageSelector() {
+    /* The page selector must be updated differently on the beauty shot and video showcase tabs. */
+    if(displayMode == "beautyShot") {
+
+        maxPageNumber = showcaseCar.imageLimitBea;
+
+    } else if(displayMode == "video") {
+
+        maxPageNumber = showcaseCar.videoLimit;
+
+    } else {
+
+        var i = 0;
+        /* Finding all pages with text */
+        do {
+            console.log("Found text in slot " + i);
+            i++;
+        }
+        while(currentShowcase[i] != null);
+
+        maxPageNumber = i;
+    }
+
+    document.getElementById("porscheModel").innerHTML = currentPageNumber + "/" + maxPageNumber;
+    updateArrows(currentPageNumber, maxPageNumber);
+}
+
+/* This function changes pages on every showcase tab. */
 function changePage(value) {
     if(currentPageNumber + value > 0 && currentPageNumber + value <= maxPageNumber) {
         currentPageNumber = currentPageNumber + value;
     }
 
-    document.getElementById("porscheShowcase").innerHTML = currentShowcase[currentPageNumber-1];
-    if(currentHeader instanceof Array) {
-        document.getElementById("porscheHeader").innerHTML = currentHeader[currentPageNumber-1];
-    }
+    document.getElementById("porscheModel").innerHTML = currentPageNumber + "/" + maxPageNumber;
 
-    updatePageSelector();
+    applyPageChangeData();
+    updateArrows(currentPageNumber, maxPageNumber);
 }
 
-/* This function handles the current page value */
-function updatePageSelector() {
-    var i = 0;
-    /* Finding all pages with text */
-    do {
-        console.log("Found text in slot " + i);
-        i++;
+/* I only want to modify the showcase text on screen if the display mode is normal. Otherwise the text is not visible. */
+function applyPageChangeData() {
+    if(displayMode == "normal") {
+        document.getElementById("porscheShowcase").innerHTML = currentShowcase[currentPageNumber-1];
+        if(currentHeader instanceof Array) {
+            document.getElementById("porscheHeader").innerHTML = currentHeader[currentPageNumber-1];
+        }
     }
-    while(currentShowcase[i] != null);
-
-    document.getElementById("porscheModel").innerHTML = currentPageNumber + "/" + i;
-    maxPageNumber = i;
-    updateArrows(currentPageNumber, maxPageNumber);
 }
 
 /* This function refreshes the state of the arrows used to change showcase text page. */
@@ -159,49 +201,27 @@ function findCurrentImagetypes() {
         case "description":
             currentPageIds.push("gen");
             currentPageIds.push("sml");
-            currentHeader = "Overview";
-            currentShowcase = showcaseCar.overview;
-            changeView(true);
             break;
         case "specSheet":
             currentPageIds.push("cut");
             currentPageIds.push("eng");
-            currentHeader = ["General", "Performance", "Mechanical", "Drivetrain", "Dimensions & Chassis"]
-            currentShowcase = showcaseCar.specs;
-            changeView(true);
             break;
         case "beautyShots":
             currentPageIds.push("bea");
             currentPageIds.push("bea");
-            currentHeader = "Beauty Shots"
-            changeView(false);
             break;
         case "video":
             /* I don't yet know what should be here. */
             currentPageIds.push("vid");
             currentPageIds.push("vid");
-            currentHeader = "Showcase"
-            changeView(false);
             break;
         default:
             currentPageIds.push("gen");
             currentPageIds.push("sml");
-            currentHeader = "Overview";
     }
 
     applyImageLimits(currentPageIds[0], currentPageIds[1]);
     console.log(currentPageIds);
-}
-
-/* This function changes between the two porscheThumbnails used to present the showcase. */
-function changeView(style) {
-    if(style) {
-        document.getElementById("porscheThumbnail").style.display = "flex";
-        document.getElementById("porscheThumbnail2").style.display = "none";
-    } else {
-        document.getElementById("porscheThumbnail").style.display = "none";
-        document.getElementById("porscheThumbnail2").style.display = "flex";
-    }
 }
 
 /* Since every image type has an individual maximum amount, the page's current limits for big and small images must be dynamically changed based on the page we're on. */
@@ -241,6 +261,31 @@ function applyImageLimits(big, small) {
     }
 }
 
+/* This function changes between the two porscheThumbnails used to present the showcase. */
+function changeView(style) {
+    var normal = document.getElementById("porscheThumbnail");
+    var beauty = document.getElementById("porscheThumbnailBea");
+    var video = document.getElementById("porscheThumbnailVid");
+
+    switch(style) {
+        case "normal":
+            normal.style.display = "flex";
+            beauty.style.display = "none";
+            video.style.display = "none";
+            break;
+        case "beautyShot":
+            normal.style.display = "none";
+            beauty.style.display = "flex";
+            video.style.display = "none";
+            break;
+        case "video":
+            normal.style.display = "none";
+            beauty.style.display = "none";
+            video.style.display = "flex";
+            break;
+    }
+}
+
 /* selectPreviousImage requires a parameter, which can't be entered in an event listener. */
 function selectPreviousBigImage() {
     selectImage(true, -1)
@@ -259,10 +304,16 @@ function selectNextSmallImage() {
 }
 
 function changePageDown() {
+    if(displayMode == "beautyShot" || displayMode == "video") {
+        selectPreviousBigImage();
+    }
     changePage(-1);
 }
 
 function changePageUp() {
+    if(displayMode == "beautyShot" || displayMode == "video") {
+        selectNextBigImage();
+    }
     changePage(1);
 }
 
@@ -295,7 +346,11 @@ function selectImage(big, addedIndex) {
         currentSmallImage = currentImage;
     }
 
-    findShowcaseImageOfType(big, currentCar, currentImage);
+    if(displayMode != "video") {
+        findShowcaseImageOfType(big, currentCar, currentImage);
+    } else {
+        document.getElementById("porscheVideo").src = showcaseCar.videos[currentImage-1];
+    }
 }
 
 /* This function finds images from the images_showroom folder and applies them to their respective containers. */
@@ -303,18 +358,22 @@ function findShowcaseImageOfType(big, carName, index) {
     var type = 0;
     var id = "";
 
-    if(big) {
-        type = 0;
-        id = "porscheImage";
+    if(displayMode == "normal") {
+        if(big) {
+            type = 0;
+            id = "porscheImage";
+        } else {
+            type = 1;
+            id = "porscheImage2";
+        }
     } else {
-        type = 1;
-        id = "porscheImage2";
+        type = 0;
+        id = "porscheImageBea";
     }
 
     var newUrl = 'images_showcase/' + carName + '/' + carName + '_' + currentPageIds[type] + '_0' + index + '.jpg';
     console.log("Showing: " + newUrl);
     document.getElementById(id).src = newUrl;
-    document.getElementById("beaImageContainer").style.backgroundImage = "url(" + newUrl + ")";
 }
 
 /* Mostly a copy of the same function in the timeline page. It changes page when you press the tabs at the top. */
@@ -326,22 +385,38 @@ function changeCurrentFamily() {
 
 /* This function will have to do the last function's heavy lifting. */
 function selectShowcaseTab(id) {
+    currentBigImage = 1;
+    currentSmallImage = 1;
     switch(id) {
         case "description":
             findCurrentImagetypes(id);
+            currentHeader = "Overview";
+            currentShowcase = showcaseCar.overview;
+            displayMode = "normal";
             break;
         case "specSheet":
             findCurrentImagetypes(id);
+            currentHeader = ["General", "Performance", "Mechanical", "Drivetrain", "Dimensions & Chassis"]
+            currentShowcase = showcaseCar.specs;
+            displayMode = "normal";
             break;
         case "beautyShots":
             findCurrentImagetypes(id);
+            currentHeader = "Beauty Shots"
+            displayMode = "beautyShot";
             break;
         case "video":
             findCurrentImagetypes(id);
+            currentHeader = "Showcase"
+            displayMode = "video";
+            changeView(false);
             break;
         default:
             findCurrentImagetypes("description");
+            currentHeader = "Overview";
+            displayMode = "normal"
     }
+    changeView(displayMode);
     initializeShowcase();
 }
 
@@ -371,8 +446,8 @@ constructShowcase(currentCar);
 window.onload = function() {
     instateScreenWidthHeight();
     setImageBackgrounds();
-    findCurrentImagetypes("description");
-    initializeShowcase();
+    setHeader();
+    selectShowcaseTab("description");
     document.getElementById("arrowExtension").addEventListener('click', selectPreviousBigImage);
     document.getElementById("arrowExtensionRight").addEventListener('click', selectNextBigImage);
     document.getElementById("arrowExtension2").addEventListener('click', selectPreviousSmallImage);
